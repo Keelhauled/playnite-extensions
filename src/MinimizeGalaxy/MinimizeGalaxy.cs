f48b980c2f9982ffecece5efa4c86413cd16b772
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2021-07-13 01:30:03 +0300

    MinimizeGalaxy

---
+++ src/MinimizeGalaxy/MinimizeGalaxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+
+[assembly: AssemblyFileVersion("1.0.0")]
+
+namespace MinimizeGalaxy
+{
+    public class MinimizeGalaxy : Plugin
+    {
+        private readonly ILogger logger;
+        public override Guid Id { get; } = new Guid("E314923B-A716-4500-8DB3-A11F7E253A7F");
+        
+        public MinimizeGalaxy(IPlayniteAPI api) : base(api)
+        {
+            logger = api.CreateLogger();
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            if(game.Source.Name == "GOG")
+            {
+                foreach(var proc in Process.GetProcessesByName("GalaxyClient"))
+                {
+                    logger.Info($"Minimizing GOG Galaxy ({proc.Id})");
+                    proc.CloseMainWindow();
+                }
+            }
+        }
+    }
+}
